38ffb38af03213e43c86e571f32cdcf22da224e4
Sanshiro Yoshida <halcat0x15a@gmail.com>
2015-03-10 10:11:50 +0900

    move anyval instance into std

--- src/main/scala/kits/std/package.scala
+++ src/main/scala/kits/std/package.scala
@@ -3,6 +3,22 @@ package kits
 import scala.concurrent.{Future, ExecutionContext}
 
 package object std {
+  implicit def sum[A](implicit A: Numeric[A]) = new Monoid[A] {
+    def zero: A = A.zero
+    def append(x: A, y: A): A = A.plus(x, y)
+  }
+  implicit def product[A](implicit A: Numeric[A]) = new Monoid[A] {
+    def zero: A = A.one
+    def append(x: A, y: A): A = A.times(x, y)
+  }
+  implicit val all = new Monoid[Boolean] {
+    def zero: Boolean = true
+    def append(x: Boolean, y: Boolean): Boolean = x && y
+  }
+  implicit val any = new Monoid[Boolean] {
+    def zero: Boolean = false
+    def append(x: Boolean, y: Boolean): Boolean = x || y
+  }
   implicit val string = new Monoid[String] {
     def zero: String = ""
     def append(x: String, y: String): String = x + y
