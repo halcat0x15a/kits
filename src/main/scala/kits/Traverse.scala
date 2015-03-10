d710058d41e9e13f04eaa44831ec504952f16fc3
Sanshiro Yoshida <halcat0x15a@gmail.com>
2015-03-10 20:29:54 +0900

    add fold method

--- src/main/scala/kits/Traverse.scala
+++ src/main/scala/kits/Traverse.scala
@@ -5,4 +5,5 @@ trait Traverse[F[_]] extends Functor[F] {
   def sequence[G[_]: Applicative, A](fga: F[G[A]]): G[F[A]] = traverse(fga)(identity)
   def map[A, B](fa: F[A])(f: A => B): F[B] = traverse[Id, A, B](fa)(f)
   def foldMap[A, B: Monoid](fa: F[A])(f: A => B): B = traverse[({ type F[A] = Const[B, A] })#F, A, B](fa)(f)
+  def fold[A: Monoid](fa: F[A]): A = foldMap(fa)(identity)
 }
