84b6fe9d5374a9d0dcdbe04d00c15559ead3ebbe
Sanshiro Yoshida <halcat0x15a@gmail.com>
2015-03-16 11:11:35 +0900

    add useful functions

--- src/main/scala/kits/Applicative.scala
+++ src/main/scala/kits/Applicative.scala
@@ -5,3 +5,10 @@ trait Applicative[F[_]] extends Functor[F] {
   def apply[A, B](fa: F[A])(f: F[A => B]): F[B]
   def map[A, B](fa: F[A])(f: A => B): F[B] = apply(fa)(pure(f))
 }
+
+object Applicative {
+  def apply[F[_], A, B](fa: F[A])(f: F[A => B])(implicit F: Applicative[F]): F[B] = F(fa)(f)
+  def map[F[_], A, B](fa: F[A])(f: A => B)(implicit F: Applicative[F]): F[B] = F.map(fa)(f)
+  def map[F[_]: Applicative, A, B, C](fa: F[A], fb: F[B])(f: (A, B) => C): F[C] = apply(fb)(map(fa)(a => f(a, _)))
+  def map[F[_]: Applicative, A, B, C, D](fa: F[A], fb: F[B], fc: F[C])(f: (A, B, C) => D): F[D] = apply(fc)(map(fa, fb)((a, b) => f(a, b, _)))
+}
