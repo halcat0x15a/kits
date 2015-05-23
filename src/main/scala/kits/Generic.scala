982573883a99bbab6ee902aa6e943561983f7b62
Sanshiro Yoshida <halcat0x15a@gmail.com>
2015-05-23 17:28:07 +0900

    update generic rep

--- src/main/scala/kits/Generic.scala
+++ src/main/scala/kits/Generic.scala
@@ -4,7 +4,7 @@ import scala.language.experimental.macros
 
 import generic._
 
-case class Meta[T, A](a: A) extends AnyVal
+case class Meta[T, A](value: A)(implicit val tag: scala.reflect.ClassTag[T])
 
 trait Generic[A] {
   type Rep
@@ -20,16 +20,12 @@ private class GenericMacros(val c: scala.reflect.macros.whitebox.Context) {
 
   import c.universe._, internal._
 
-  val left = symbolOf[Left[_, _]].companion
-
-  val right = symbolOf[Right[_, _]].companion
-
   def apply[A: c.WeakTypeTag]: Tree = {
     val tpe = weakTypeOf[A]
     val sym = tpe.typeSymbol.asClass
-    val rep = sumType(sym, tpe)
-    val from = exprs(sym, false)
-    val to = exprs(sym, true)
+    val rep = sum(sym, tpe)
+    val from = cases(sym, false)
+    val to = cases(sym, true)
     q"""
 new ${symbolOf[Generic[_]]}[$tpe] {
   type Rep = $rep
@@ -39,44 +35,41 @@ new ${symbolOf[Generic[_]]}[$tpe] {
 """
   }
 
-  def exprs(sym: ClassSymbol, isExpr: Boolean): List[Tree] =
-    for ((a, r) <- constructorTree(sym, isExpr).zip(sumTree(sym, !isExpr))) yield
+  def cases(sym: ClassSymbol, isExpr: Boolean): List[Tree] =
+    for ((a, r) <- constructor(sym, isExpr).zip(sum(sym, !isExpr))) yield
       if (isExpr)
         cq"$r => $a"
       else
         cq"$a => $r"
 
-  def sumType(sym: ClassSymbol, tpe: Type): Type =
-    metaType(sym, constructors(sym).map(productType(_, tpe)).foldRight(typeOf[Void])(appliedType(typeOf[:+:[_, _]], _, _)))
+  def sum(sym: ClassSymbol, tpe: Type): Type =
+    meta(tpe, constructors(sym).map(product(_, tpe)).foldRight(typeOf[Void])(appliedType(typeOf[:+:[_, _]], _, _)))
 
-  def productType(sym: ClassSymbol, tpe: Type): Type =
-    metaType(sym, parameterTypes(sym, tpe).foldRight(typeOf[Unit])(appliedType(typeOf[:*:[_, _]], _, _)))
+  def sum(sym: ClassSymbol, isExpr: Boolean): List[Tree] =
+    constructors(sym).foldRight(List(q"${symbolOf[Void].companion}"))((a, b) => q"${symbolOf[Left[_, _]].companion}(${product(a, isExpr)})" :: b.map(x => q"${symbolOf[Right[_, _]].companion}($x)")).map(meta)
 
-  def parameterTypes(sym: ClassSymbol, tpe: Type): List[Type] =
-    parameters(sym).map(param => metaType(param, param.info.substituteTypes(sym.typeParams, tpe.typeArgs)))
+  def product(sym: ClassSymbol, tpe: Type): Type =
+    meta(sym.selfType.substituteTypes(sym.typeParams, tpe.typeArgs), parameters(sym).map(_.info.substituteTypes(sym.typeParams, tpe.typeArgs)).foldRight(typeOf[Unit])(appliedType(typeOf[:*:[_, _]], _, _)))
 
-  def metaType(sym: Symbol, tpe: Type): Type =
-    appliedType(typeOf[Meta[_, _]], constantType(Constant(sym.name.decodedName.toString)), tpe)
+  def product(sym: ClassSymbol, isExpr: Boolean): Tree =
+    meta(parameter(sym, isExpr).foldRight(q"${symbolOf[Unit].companion}")((a, b) => q"${symbolOf[:*:[_, _]].companion}($a, $b)"))
 
-  def sumTree(sym: ClassSymbol, isExpr: Boolean): List[Tree] =
-    constructors(sym).foldRight(List(q"${symbolOf[Void]}"))((a, b) => q"$left(${productTree(a, isExpr)})" :: b.map(x => q"$right($x)")).map(metaTree)
+  def meta(tag: Type, value: Type): Type =
+    appliedType(typeOf[Meta[_, _]], tag, value)
 
-  def productTree(ctor: ClassSymbol, isExpr: Boolean): Tree =
-    metaTree(parameterTrees(ctor, isExpr).foldRight(q"${symbolOf[Unit].companion}")((a, b) => q"${symbolOf[:*:[_, _]].companion}(${metaTree(a)}, $b)"))
-
-  def metaTree(tree: Tree): Tree =
+  def meta(tree: Tree): Tree =
     q"${symbolOf[Meta[_, _]].companion}($tree)"
 
-  def constructorTree(sym: ClassSymbol, isExpr: Boolean): List[Tree] =
+  def constructor(sym: ClassSymbol, isExpr: Boolean): List[Tree] =
     for (ctor <- constructors(sym)) yield
       if (ctor.isModuleClass)
         q"${ctor.module}"
       else
-        q"${ctor.companion}(..${parameterTrees(ctor, isExpr)})"
+        q"${ctor.companion}(..${parameter(ctor, isExpr)})"
 
-  def parameterTrees(ctor: ClassSymbol, isExpr: Boolean): List[Tree] =
+  def parameter(sym: ClassSymbol, isExpr: Boolean): List[Tree] =
     for {
-      param <- parameters(ctor)
+      param <- parameters(sym)
       name = param.asTerm.name
     } yield if (isExpr) q"$name" else pq"$name@_"
 
@@ -90,7 +83,7 @@ new ${symbolOf[Generic[_]]}[$tpe] {
       Nil
   }
 
-  def parameters(ctor: ClassSymbol): List[Symbol] =
-    ctor.primaryConstructor.asMethod.paramLists.head
+  def parameters(sym: ClassSymbol): List[Symbol] =
+    sym.primaryConstructor.asMethod.paramLists.head
 
 }
