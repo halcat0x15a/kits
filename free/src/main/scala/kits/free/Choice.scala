d6db040d7d93d2895c108f3d3b1fce62d0d07923
Sanshiro Yoshida <halcat0x15a@gmail.com>
2016-03-20 18:04:27 +0900

    add split to Choice

--- free/src/main/scala/kits/free/Choice.scala
+++ free/src/main/scala/kits/free/Choice.scala
@@ -20,4 +20,15 @@ object Choice {
 
   def plus[U <: Union, M[_], A](x: Free[U, A], y: Free[U, A])(implicit F: Member[Choice[M], U]): Free[U, A] = Free[U, Boolean](F.inject(Plus())).flatMap(if (_) x else y)
 
+  def reflect[U <: Union, M[_], A](option: Option[(A, Free[U, A])])(implicit F: Member[Choice[M], U]): Free[U, A] =
+    option.fold(zero: Free[U, A]) {
+      case (a, free) => plus(Pure(a), free)
+    }
+
+  def split[U <: Union, M[_], A](free: Free[U, A])(implicit F: Member[Choice[M], U]): Free[U, Option[(A, Free[U, A])]] =
+    Free.interpose(free)(a => Pure(Some((a, zero)): Option[(A, Free[U, A])]))((_: Choice[M]) match {
+      case Zero() => _ => Pure(None)
+      case Plus() => k => for (x <- k(true); y <- k(false)) yield x.fold(y) { case (a, f) => Some((a, plus(f, reflect(y)))) }
+    })
+
 }
