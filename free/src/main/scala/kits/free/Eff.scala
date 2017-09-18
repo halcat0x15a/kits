d7680989e725ed5537c6fc59959527b16e76555f
sanshiro_yoshida <sanshiro_yoshida@dwango.co.jp>
2017-09-18 13:03:29 +0900

    binary tree

--- free/src/main/scala/kits/free/Eff.scala
+++ free/src/main/scala/kits/free/Eff.scala
@@ -2,36 +2,40 @@ package kits.free
 
 trait Eff[A] { self =>
 
-  type Constraints <: HList
+  type Effects <: HTree
 
-  def free[U](implicit members: MemberList[Constraints, U]): Free[U, A]
+  def free[U](implicit members: MemberSet[Effects, U]): Free[U, A]
 
-  def map[B](f: A => B): Eff[B] { type Constraints = self.Constraints } = ???
+  def map[B](f: A => B) = new Eff[B] {
+    type Effects = self.Effects
+    def free[U](implicit members: MemberSet[Effects, U]) = self.free[U].map(f)
+  }
 
-  def flatMap[B, L <: HList](f: A => Eff[B] { type Constraints = L }) = new Eff[B] {
-    type Constraints = self.Constraints#Append[L]
-    def free[U](implicit members: MemberList[Constraints, U]): Free[U, B] =
-      self.free[U].flatMap(a => f(a).free[U])
+  def flatMap[B, L <: HTree](f: A => Eff[B] { type Effects = L }) = new Eff[B] {
+    type Effects = self.Effects :*: L
+    def free[U](implicit members: MemberSet[Effects, U]): Free[U, B] =
+      self.free[U](MemberSet.left).flatMap(a => f(a).free[U](MemberSet.right))
   }
 
 }
 
 object Eff {
 
-  def ask[R]: Eff[R] { type Constraints = :*:[Reader[R], HNil] } = new Eff[R] {
-    type Constraints = :*:[Reader[R], HNil]
-    def free[U](implicit members: MemberList[Constraints, U]): Free[U, R] = Reader.ask[U, R](MemberList.head)
+  def ask[R] = new Eff[R] {
+    type Effects = HLeaf[Reader[R]]
+    def free[U](implicit members: MemberSet[Effects, U]): Free[U, R] = Reader.ask(MemberSet.get)
   }
 
-  def tell[W](w: W): Eff[Unit] { type Constraints = :*:[Writer[W], HNil] } = new Eff[Unit] {
-    type Constraints = :*:[Writer[W], HNil]
-    def free[U](implicit members: MemberList[Constraints, U]): Free[U, Unit] = Writer.tell(w)(MemberList.head)
+  def tell[W](w: W) = new Eff[Unit] {
+    type Effects = HLeaf[Writer[W]]
+    def free[U](implicit members: MemberSet[Effects, U]): Free[U, Unit] = Writer.tell(w)(MemberSet.get)
   }
 
 }
 
-object Main {
+object Main extends App {
   import Eff._
-  type U = Reader[String] :+: Writer[String] :+: Void
-  (for { x <- ask[String]; _ <- tell(x); _ <- tell(x); _ <- ask[String] } yield x).free[U]
+    println((Writer.handle[String] compose Reader.handle("hoge")).run {
+    (for { x <- ask[String]; _ <- tell(x); _ <- tell(x); _ <- ask[String] } yield x).free
+  })
 }
